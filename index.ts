89bcf4e60594cfcf730a91d3af469dd8c3a3938f
jonesp1 <jonas.poulsen@lego.com>
2024-10-10 13:48:08 +0200

    negative space programming assertion

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import { strict as assert } from 'assert';
+type Foo = {
+    bar?: number;
+};
+
+function morphFoo(foo: Foo): number {
+    assert(foo.bar !== undefined, "foo must exist");
+    return foo.bar! * 5;
+}
+
+
+const foo1: Foo = { bar: 2 };
+console.log(morphFoo(foo1));
+
+const foo2: Foo = {};
+console.log(morphFoo(foo2));
